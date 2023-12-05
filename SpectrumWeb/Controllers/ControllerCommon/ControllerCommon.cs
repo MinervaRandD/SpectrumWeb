bf9b236a99f604bf035199f869b6a53960a16e1f
MinervaRandD <marc.d.diamond@minervarandd.com>
2023-12-05 17:18:50 -0500

    Check in after work on layout

---
+++ SpectrumWeb/Controllers/ControllerCommon/ControllerCommon.cs
@@ -0,0 +1,58 @@
+﻿namespace SpectrumWeb.Controllers.ControllerCommon
+{
+
+    public static class ControllerCommon
+    {
+        public static string TwoPartCustomForm(List<FieldSpec> fieldList, List<FieldSpec> childFieldList)
+        {
+            string rtrnValue = "<div id='customForm' class='row'>\n";
+
+            rtrnValue += "    <div class='col'>\n";
+
+            foreach (FieldSpec fieldSpec in fieldList)
+            {
+                if (isChildRow(fieldSpec, childFieldList))
+                {
+                    continue;
+                }
+
+                rtrnValue += "         <editor-field name='" + fieldSpec.field + "'></editor-field>\n";
+            }
+
+            rtrnValue += "    </div>\n";
+
+            rtrnValue += "    <div class='col'>\n";
+
+            foreach (FieldSpec fieldSpec in childFieldList)
+            {
+                rtrnValue += "         <editor-field name='" + fieldSpec.field + "'";
+                
+                if (fieldSpec.fieldHeight != null)
+                {
+                    rtrnValue += " style='height:" + fieldSpec.fieldHeight.Value + "px'";
+                }
+
+                rtrnValue += "></editor-field>\n";
+            }
+
+            rtrnValue += "    </div>\n";
+
+            rtrnValue += "</div>";
+
+            return rtrnValue;
+        }
+
+
+        public static bool isChildRow(FieldSpec fieldSpec, List<FieldSpec> childFieldList)
+        {
+            if (childFieldList is null)
+            {
+                return false;
+            }
+
+            return childFieldList.Any(x => x.field == fieldSpec.field);
+
+        }
+
+    }
+}
